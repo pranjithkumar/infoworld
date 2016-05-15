01055f8afc2b3197626d570770f5a85ea4774643
sivakavi Jayadevan <sivakavij@ihorsetech.com>
2016-05-16 00:02:28 +0530

    producation enabled

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import {provide} from '@angular/core';
+import {provide, enableProdMode} from '@angular/core';
 import {Http, HTTP_PROVIDERS} from '@angular/http';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {AppComponent} from './components/app.component/app.component';
@@ -8,13 +8,15 @@ import {ToastOptions} from "ng2-toastr/ng2-toastr";
 
 
 let options = {
-      positionClass: 'toast-bottom-right',
-    };
+    positionClass: 'toast-bottom-right',
+};
+
+enableProdMode();
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     HTTP_PROVIDERS,
     provide(ROUTER_PROVIDERS, { useValue: '/' }),
-    HttpServices
-    provide(ToastOptions, { useValue: new ToastOptions(options)}),
+    HttpServices,
+    provide(ToastOptions, { useValue: new ToastOptions(options) }),
 ]);
